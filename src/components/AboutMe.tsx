36f63fd0436031bfd6c3b17ff7f4fffb0cb08b12
Peter De Clercq <peterdeclercq1997@outlook.com>
2023-09-07 22:04:19 +0200

    💬 Add content to about me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Asset2 from "@/assets/images/asset2.svg";
 import Logo from "@/assets/images/logo.svg";
 import styles from "@/styles/AboutMe.module.css";
 import { motion } from "framer-motion";
@@ -7,8 +8,6 @@ import { Livvic } from "next/font/google";
 import Moramit from "next/font/local";
 import Image from "next/image";
 import { useState } from "react";
-import Asset2 from "@/assets/images/asset2.svg";
-import ActionButton from "./ActionButton";
 
 const moramit = Moramit({ src: "./../assets/fonts/Moramit.otf" });
 
@@ -107,44 +106,74 @@ const AboutMe = () => {
               viewport={{ once: true }}
             >
               {currentTab === "Education" ? (
-                <motion.p
-                  initial={{ opacity: 0, translateX: 100 }}
-                  whileInView={{ opacity: 1, translateX: 0 }}
-                  transition={{ ease: "easeOut" }}
-                  viewport={{ once: true }}
-                >
-                  I&apos;ve had 5 years of experience studying Applied
-                  Informatics - with a heavy focus on front-end development.
-                  Right before finishing my final internship, I had doubts about
-                  continuing this path, so I took a break from it all. I took a
-                  1 year break to think about it, only to realize that starting
-                  a career as a front-end developer is truly where my passion
-                  lies. I have no degree, but I have tons of enthusiasm and
-                  know-how to get back into the game!
-                </motion.p>
+                <div>
+                  <motion.p
+                    initial={{ opacity: 0, translateX: 100 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
+                    transition={{ ease: "easeOut" }}
+                    viewport={{ once: true }}
+                  >
+                    I&apos;ve had 5 years of experience studying Applied
+                    Informatics - with a heavy focus on front-end development.
+                    Right before finishing my final internship, I had doubts
+                    about continuing this path, so I took a break from it all. I
+                    took a 1 year break to think about it, only to realize that
+                    starting a career as a front-end developer is truly where my
+                    passion lies. I have no degree, but I have tons of
+                    enthusiasm and know-how to get back into the game!
+                  </motion.p>
+                </div>
               ) : null}
               {currentTab === "Life" ? (
-                <motion.p
-                  initial={{ opacity: 0, translateX: 100 }}
-                  whileInView={{ opacity: 1, translateX: 0 }}
-                  transition={{ ease: "easeOut" }}
-                  viewport={{ once: true }}
-                >
-                  Life text
-                </motion.p>
+                <div>
+                  <motion.p
+                    initial={{ opacity: 0, translateX: 100 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
+                    transition={{ ease: "easeOut" }}
+                    viewport={{ once: true }}
+                  >
+                    I have a mix of backgrounds in my family. My mom is from
+                    Ecuador, and my dad is Belgian. This mix has exposed me to
+                    different customs and foods from both cultures. It&apos;s a
+                    big part of who I am, and I really enjoy exploring and
+                    learning from these diverse influences.
+                  </motion.p>
+                </div>
               ) : null}
               {currentTab === "Nice-to-knows" ? (
-                <motion.p
-                  initial={{ opacity: 0, translateX: 100 }}
-                  whileInView={{ opacity: 1, translateX: 0 }}
-                  transition={{ ease: "easeOut" }}
-                  viewport={{ once: true }}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
                 >
-                  They say the human body is 80% water. Well, mine is a little
-                  different. It&apos;s 79% water, 1% titanium, and 20% sheer
-                  determination to never back down. Because who needs a flexible
-                  spine when you&apos;ve got a titanium one, right?
-                </motion.p>
+                  <motion.p
+                    initial={{ opacity: 0, translateX: 100 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
+                    transition={{ ease: "easeOut" }}
+                    viewport={{ once: true }}
+                    style={{ paddingBottom: "10px" }}
+                  >
+                    They say the human body is 80% water. Well, mine is a little
+                    different. It&apos;s 79% water, 1% titanium, and 20% sheer
+                    determination to never back down. Because who needs a
+                    flexible spine when you&apos;ve got a titanium one, right?
+                  </motion.p>
+
+                  <motion.p
+                    initial={{ opacity: 0, translateX: 100 }}
+                    whileInView={{ opacity: 1, translateX: 0 }}
+                    transition={{ ease: "easeOut" }}
+                    viewport={{ once: true }}
+                  >
+                    Instead of traditional TV shows, I often find myself
+                    exploring unique and imaginative worlds through anime.
+                    Whether it&apos;s the pirate adventures of One Piece or the
+                    rich worldbuilding of Naruto, I&pos;m drawn to the limitless
+                    creativity and storytelling found in anime.
+                  </motion.p>
+                </div>
               ) : null}
             </motion.div>
             <motion.div
@@ -154,12 +183,13 @@ const AboutMe = () => {
               transition={{ delay: 1, ease: "easeOut" }}
               viewport={{ once: true }}
             >
-              <a
+              <motion.a
                 className={styles.button}
+                whileHover={{ scale: 1.1 }}
                 href="https://drive.google.com/uc?export=download&id=1i0xDKTci6SUYoqeVndAMgkHFMss_9cx0"
               >
                 Download CV
-              </a>
+              </motion.a>
             </motion.div>
           </div>
         </div>
